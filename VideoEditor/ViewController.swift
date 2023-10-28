665eb4764aeb3cb313b5c3c08167f9df75580a98
Ualikhan <usabden@phoenixit.ru>
2023-10-28 15:14:16 +0600

    Initial Commit

---
+++ VideoEditor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoEditor
+//
+//  Created by Ualikhan Sabden on 28.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
